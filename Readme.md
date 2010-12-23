c5339fabe69fbcfbf9b2e8e7b4beea911b9feb7e
Gregor Schwab <isimpl@gmail.com>
2010-12-23 11:39:08 +0100

    added hoverable

--- Readme.md
+++ Readme.md
@@ -1,31 +1,59 @@
 # Touchable #
 
-Touchable is a very lightweight jQuery Class that unifies touch and mouse events over different platforms like desktops and mobile devices with touchscreens (like Android, iPad, iPod Touch, iPhone etc.)
+Touchable is a very lightweight (3,12kb) jQuery Class that unifies touch and mouse events over different platforms like desktops and mobile devices with touchscreens (like Android, iPad, iPod Touch, iPhone etc.)
 The name Touchable therefore might be a bit confusing, cause we actually generate a generic "Touch" which involves also mouse events. For convenience they are all called "Touches".
 
 Touchable really doesn't depend that much on jQuery, so it should work with other libraries like [Zepto][] too. Havent tested that though.
 [Zepto]: http://zeptojs.com/
 
+# Hoverable #
+
+Hoverable is built upon Touchable and is a very lightweight (1,7kB) jQuery Class that unifies hover events over different platforms like desktops and mobile devices with touchscreens (like Android, iPad, iPod Touch, iPhone etc.)
+It introduces a new event called 
+
+* **newHover(2)**: following my [blog][] article about UI design and my postulate, that Long Tap is the new Hover. Fired when the user hovers with the mouse or longTaps an element. Hovever if you want to you can set it up to a touchmove event too, which will fire a genericHover2 event.
+
+
+##Testing##
+right now I have tested Touchable on the following devices and browsers Chrome, Firefox, Safari, iPad Simulator, iPad, iPhone. But it should work quite everywhere. If you have any bug notes drop me a line.
+
+##Demo##
+I have setup a demo site for Hoverable on [connect-mi.com][]
+[connect-mi.com]: http://connect-mi.com/
+
 ## Usage ##
 
-Right now Touchable supports five basic events, all events when bound with jQuery's bind function get a touch object passed in as second argument
+Right now Touchable supports five basic events and Hoverable adds two generic Hover events, all events when bound with jQuery's bind function get a touch object passed in as second argument
 
-* **touchmove**: fired when the user touched or clicked with the mouse and moves to another position. Right now Touchable supports up to 2 fingers. If 2 fingers are on screen the lower left is taken as a reference.
-* **touchend**: fired when the user ended a touch
+TOUCHABLE
+==========
+* **touchablemove**: fired when the user touched or clicked with the mouse and moves to another position. Right now Touchable supports up to 2 fingers. If 2 fingers are on screen the lower left is taken as a reference.
+* **touchableend**: fired when the user ended a touch
 * **tap**: fired when the user clicks with his mouse or taps with his finger
 * **longTap**: fired when the user stays with his mouse or finger on an item for 1 second
 * **doubleTap**: fired when the user taps two times within half of a second
+[blog]: http://grenzgenial.com/
+[ajaxian]: http://ajaxian.com/archives/mouseovers-on-touch-devices
+
+HOVERABLE
+==========
+* **newHover(2)**: Fired when the user hovers with the mouse or longTaps an element. Hovever if you want to you can set it up to a touchmove event too, which will fire a genericHover2 event.
 
 
 you use it by initializing Touchable on a view element, like so:
 
     var div = $(<div>).Touchable();
+
+or
+
+    var div = $(<div>).Hoverable(); //which cretaes a Touchable internally
     
 then you bind to the events
 
     div.bind('touchmove', function(e, touch){})
+    div.bind('newHover2', function in(e, touch){}, function out(e, touch){})
     
-notice, that each event gets passed a touch object, besides the normal event object. The Touch object has the following properties:
+notice, that each event gets passed a touch object, besides the normal event object. The Touch object has the following properties: 
 
 
 * startTouch: this is where the touch or mousedown event originated
---
+++ hoverable.js
@@ -0,0 +1,144 @@
+/*
+ * jQuery Hoverable
+ *
+ * Simplified BSD License (@see License)
+ * @author        Gregor Schwab
+ * @copyright     (c) 2010 Gregor Schwab
+ * Usage Command Line: $(elem).Touchable() (@see Readme.md)
+ * @requires jQuery Touchable
+ */
+
+(function($) {
+  var Touchable=$.Touchable;
+  $.fn.Hoverable = function(conf) {
+      return this.each(function() {       
+      var t= $(this).data['Hoverable']=new Hoverable(this, conf);
+      return t;
+    });
+  }
+  $.fn.newHover = function(fn1, fn2, disableHover) {
+      return this.each(function() {
+        $(this).bind('newHoverIn', fn1).bind('newHoverOut', fn2);
+    });
+  }  
+  $.fn.newHover2 = function(fn1, fn2) {
+      return this.each(function() {
+        $(this).bind('newHoverIn2', fn1).bind('newHoverOut2', fn2);
+    });
+  }  
+  /**
+   * @constructor
+   */  
+  function Hoverable(elem, conf)
+  {
+    
+    this.logging=false; //set to false to disabele logging default false gets overwritten by conf see below
+    var log=function(a){if(self.logging){console.log(arguments);}} ;//private logging function
+    
+
+    this.elem=elem;    
+    try{this.$elem=$(elem).Touchable();}catch(e){console.log('Hoverable depends on Touchable! Please be sure to include Touchable in your project.')}
+
+
+    this.inHover=false;
+    this.target=null;
+
+    var self=this; 
+    
+    if (typeof conf!=='undefined'){
+      if(typeof conf.disableHover!=='undefined'){this.disableHover=conf.disableHover;}
+      else{this.disableHover=false;}
+      if(typeof conf.logging!=='undefined'){this.logging=conf.logging;}            
+    }     
+          
+     
+    //longTap is the new Hover ;)
+    if (!this.disableHover){
+      this.$elem.bind('mouseenter', genericHover);
+      this.$elem.bind('mouseleave', genericHover);      
+    }
+
+   
+    this.$elem.bind('longTap', genericHover);  
+    this.$elem.bind('touchableend', genericHover); 
+          
+    function genericHover(e, touch){
+      if(e.type==='touchableend' ||e.type==='mouseleave'){
+        log('Touchable newHoverOut');    
+        //this.$elem.unbind('touchend', genericHover);//don't have to unbind mouseleave .unbind('mouseleave', genericHover);
+        return self.$elem.trigger('newHoverOut', self);
+      }
+        log('Touchable newHoverIn');               
+				self.$elem.trigger('newHoverIn', self); //trigger a genericHover see Readme      
+    }    
+    
+    //mousemove is the new Hover ;)
+    if (!this.disableHover){
+      this.$elem.bind('mouseenter', genericHover2);
+      this.$elem.bind('mouseleave', genericHover2);     
+    }
+    self.$elem.bind('touchablestart', function(e, touch){
+      self.$elem.bind('touchablemove', genericHover2);                  
+    }, false); 
+    self.$elem.bind('touchableend', function(e, touch){
+      self.$elem.unbind('touchablemove', genericHover2);
+      genericHover2(e, touch);          
+    }, false);      
+
+ 
+    //this.$elem.bind('touchend', genericHover2);    
+    function genericHover2(e, touch){
+      if(e.type==='touchableend'||e.type==='touchend'){
+        log('Touchable newHoverOut2');
+        self.inHover=false;    
+        return self.$elem.trigger('newHoverOut2', self);
+      }else if(e.type==='mouseenter'){
+          log('Touchable newHoverIn2');
+          return self.$elem.trigger('newHoverIn2', self);
+      }else if(e.type==='mouseleave'){
+        log('Touchable newHoverOut2');          
+        return self.$elem.trigger('newHoverOut2', self);        
+      } 
+      if (e.type == 'touchablemove'){
+        if(touch instanceof Touchable){
+          var x=touch.currentTouch.x; var y=touch.currentTouch.y;  
+          var hitTarget = document.elementFromPoint(x, y);
+ /*         if(typeof hitTarget==='undefined'||hitTarget===null&&self.target!==null) {var target=self.target;var currentTarget=self.target}//just if the browser looses memory
+          else{var target=hittarget}//first the hittarget
+          if(self.logging){console.log('target '+target+' x, y:'+x,+' ' +y);}           
+        }else{
+          var target = e.target;
+        }*/
+          log('Touchable target ID/node'+ ' hitTarget'+ ' ' +
+           hitTarget+'e.target'+e.target + ' e.currentTarget'+e.currentTarget+
+           ' self in hover'+self.inHover);           
+          //lets see if we can macth our element...still playing with the right settings here cause browsers seem to have differenes in what they pass as an event.target
+          var pass=false;
+          //not good cause it goes on paragraphs but can be used to test for being outside the view element
+          //if (typeof hitTarget !== 'undefined' && hitTarget === self.$elem.get(0) ){pass=true;}
+          //if (typeof hitTarget !== 'undefined' && hitTarget !== self.$elem.get(0) ){pass=false;}
+          //gives info to the inner element too
+          //else if (typeof self.target !== 'undefined' && self.target === self.$elem.get(0)){pass=true;}//Chrome
+           //relates to the this pointer the touchmove event was bound to (normally document)
+          //else if (typeof self.currentTarget !== 'undefined' && self.currentTarget === self.$elem.get(0)){pass=true;}//iPad has it in currentTarget
+          //e.target might work too
+          //else if (typeof e.target !== 'undefined' && e.target === self.$elem.get(0)){pass=true;}    
+          //the winner is: 
+          if (typeof e.currentTarget !== 'undefined' && e.currentTarget === self.$elem.get(0)){pass=true;}                  
+          if(pass&& !self.inHover){
+            self.inHover=true;
+            log('Touchable newHoverIn2');          
+            self.$elem.trigger('newHoverIn2', self);
+            //e.stopPropagation();//we are talking about touchablemove event here          
+          }
+          else if (pass===false && self.inHover){
+            self.inHover=false;
+            log('Touchable newHoverOut2');            
+            self.$elem.trigger('newHoverOut2', self);      
+            //e.stopPropagation(); //we are talking about touchablemove event here           
+          }        
+        }
+      } 
+    }   
+  }
+})(jQuery);//end closure
