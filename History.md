83bd4296c23b560f65145eec44407c4e3cd50bbe
Gregor Schwab <isimpl@gmail.com>
2011-01-12 19:31:57 +0100

    gh-pages

---
+++ History.md
@@ -0,0 +1,13 @@
+
+0.0.1 / 2010-12-21
+------------------
+
+* Initial release
+
+
+
+0.0.2 / 2010-01-11
+------------------
+
+* Fixed bug with addEventListener not working in IE
+* merged with branch gh-pages
---
+++ gh-pages.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+var util = require('util');
+var Step = require('step');
+var exec = require('child_process').exec;
+
+//var md = require("node-markdown").Markdown;
+//var path=require('path'), fs=require('fs');
+var nmd = require('nmd')
+var errors=[];
+var verbose=true;
+var callme = function callme(command, err, stdout, stderr){
+    if(err &&  err.length==0) return;//reset the empty array passed in
+    util.print('\n'+ stylize(!err) +'output of command: '+ command + '\n');      
+    (stdout && verbose && err) && util.print('\nstdout:\n' + stdout);
+    (stderr && verbose && err) && util.print('\nstderr:\n' + stderr);      
+}
+function stylize(ok){
+  if (ok)
+    return '\033[32m' + 'OK ' +
+           '\033[39m';
+  else
+    return '\033[35m' + 'FAIL ' +
+           '\033[39m';                   
+}
+
+       
+var commands=[];
+var oldCommand="";
+var add = function(command){
+  function stepCommand(command, oldCommand){
+    return function(err, stdout, stderr){        
+      callme(oldCommand, err, stdout, stderr)
+      if(command) exec(command, this)        
+    }
+  }
+  commands.push(stepCommand(command, oldCommand))
+  oldCommand = command;
+};
+
+add('git checkout gh-pages');
+add('nmd Readme.md -o index.html');      
+add('ln -s hoverable.js demo/hoverable.js');
+add('ln -s touchable.js demo/touchable.js');
+add('git commit');
+add('git checkout master');
+add('');
+
+Step.apply(this, commands);
+
+/*exec('git checkout gh-pages', function(e, so, se){
+  console.log('')
+});*/
